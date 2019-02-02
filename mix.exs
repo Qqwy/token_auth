3d2c08a4afe975dd6b47fff8aa68ea5c825fe018
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-02-02 02:20:05 +0000

    fix/mix: version is 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TokenAuth.MixProject do
   def project do
     [
       app: :token_auth,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
