a6a75f1c0760540b6b2fc5489a9fb56dbcc11e73
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-02-02 02:18:33 +0000

    chore/mix: configure for publishing

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule TokenAuth.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -22,6 +24,20 @@ defmodule TokenAuth.MixProject do
       {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:plug, "~> 1.7.1"}
+    ]
+  end
+
+  defp description do
+    "A Plug for simple Bearer authentication"
+  end
+
+  defp package do
+    [
+      name: :token_auth,
+      files: ~w(mix.exs lib .formatter.exs README.md LICENSE),
+      maintainers: ["Jacopo Cascioli"],
+      licenses: ["MPL 2.0"],
+      links: %{"GitHub" => "https://github.com/Vesuvium/token_auth"}
     ]
   end
 end
